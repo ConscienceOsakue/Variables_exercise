6b3e558ce36e3f398ed529b9487175531ea4aeeb
conscience osakue <consciencenosa@gmail.com>
2023-11-10 15:31:25 -0600

    first commit

---
+++ Variablee_Exercise/Variablee_Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿// Declare and Initialize Variables
+string studentName = "Frank";
+int age = 27;
+char studentBloodGroup = 'A';
+bool isOver18 = true;
+double studentGpa = 3.99D;
+decimal studentMonthlyAllowance = 93.99m;
+
+
+
+
+// Initializing Variable to the console
+Console.WriteLine($" StudentName: {studentName}");
+Console.WriteLine($" Age: {age}");
+Console.WriteLine($" StudentBloodGroup: {studentBloodGroup}");
+Console.WriteLine($" Is over 18: {isOver18}");
+Console.WriteLine($" Student GPA: {studentGpa}");
+Console.WriteLine($" Student monthly allowance: {studentMonthlyAllowance}");
